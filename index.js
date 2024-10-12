f94b408c5245f326912c9c33a3e904e7f25d3ccb
Waruna <86902580+WarunaDisanayaka@users.noreply.github.com>
2024-10-13 00:18:23 +0530

    Added CORS handling

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require("express");
+const cors = require("cors"); // Import the cors middleware
 const db = require("./config/db"); // Import the database connection
 const districtRoutes = require("./routes/districtRoutes");
 const gramaNiladhariRoutes = require("./routes/gramaNiladhariRoutes");
 const userRoutes = require("./routes/userRoutes");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
-app.use(express.json()); // Middleware for parsing JSON
+// Enable CORS for all origins (you can specify your frontend URL instead if needed)
+app.use(cors());
+
+// Middleware for parsing JSON
+app.use(express.json());
 
 // Use your routes
 app.use("/api/districts", districtRoutes);
